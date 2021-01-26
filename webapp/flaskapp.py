4e6bcf6df7509fd0c9b5f673608488d75483b957
maziiw <58586260+maziiw@users.noreply.github.com>
2021-01-26 18:44:07 +1300

    Draft app

--- webapp/flaskapp.py
+++ webapp/flaskapp.py
@@ -1,15 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+posts = [
+    {
+        'ID': 'Prediction 1',
+        'author': 'air-drone-ABC',
+        'location': 'Orakei Basin',
+        'content': '95% likelihood of debris',
+        'date_captured': '2021-01-18 11:21am',
+        'image': 'p1.png'
+    },
+    {
+        'ID': 'Prediction 2',
+        'author': 'air-drone-XYZ',
+        'location': 'Orakei Marina',
+        'content': '31% likelihood of debris',
+        'date_captured': '2021-01-17 2:10pm',
+        'image': 'p2.png'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
-def hello_world():
-    return render_template('home.html')
+def home():
+    return render_template('home.html',posts=posts)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html',title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
